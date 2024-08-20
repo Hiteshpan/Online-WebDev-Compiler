1677d21cf2a2b259b1b0d5bca6ef1436839e8282
Hitesh Kumar <145363832+Hiteshpan@users.noreply.github.com>
2024-08-20 14:52:46 +0530

    Add files via upload

---
+++ client/src/pages/Compiler.tsx
@@ -0,0 +1,19 @@
+import CodeEditor from "@/components/CodeEditor";
+import HelperHeader from "@/components/HelperHeader";
+import RenderCode from "@/components/RenderCode";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
+
+export default function Compiler() {
+  return (
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel className="h-[calc(100dvh-60px)] min-w-[400px]" defaultSize={50}>
+        <HelperHeader />
+        <CodeEditor />
+      </ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel className="h-[calc(100dvh-60px)] min-w-[400px]" defaultSize={50}>
+        <RenderCode/>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  )
+}
